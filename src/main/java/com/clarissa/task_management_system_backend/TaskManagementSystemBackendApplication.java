6aa39271ca81c4308fc4235a8053b56379b02013
Clarissa <102773388@students.swinburne.edu.my>
2026-04-08 19:32:31 +0800

    Initial commit: Set up Spring Boot project structure

---
+++ src/main/java/com/clarissa/task_management_system_backend/TaskManagementSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.clarissa.task_management_system_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagementSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagementSystemBackendApplication.class, args);
+	}
+
+}
